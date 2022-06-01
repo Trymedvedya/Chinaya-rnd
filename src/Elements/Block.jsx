c500689def115511db2337e9e45a00a66c5a4ee9
Valentine <uolles.six@gmail.com>
2022-06-01 18:09:25 +0300

    adidas

--- src/Elements/Block.jsx
+++ src/Elements/Block.jsx
@@ -7,6 +7,7 @@ const Block = function (props) {
         <div onClick={props.renderer}  id="first">
           <img src={props.surs} alt="" />
         <h1 id="second">{props.name}</h1>
+        <h2 id="cumcum">Загрузка</h2>
     </div>
     )
 }
