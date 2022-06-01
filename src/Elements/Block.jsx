68102929304c9ff273b2b9ca2f2fbaefed5e058a
Valentine <uolles.six@gmail.com>
2022-06-01 22:38:52 +0300

    cum 2 back

--- src/Elements/Block.jsx
+++ src/Elements/Block.jsx
@@ -7,7 +7,6 @@ const Block = function (props) {
         <div onClick={props.renderer}  id="first">
           <img src={props.surs} alt="" />
         <h1 id="second">{props.name}</h1>
-        <h2 id="cumcum">Загрузка</h2>
     </div>
     )
 }
